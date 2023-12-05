babc0c478793b521bc94c17619b7c250b9cc6470
Alan SE <amispagel89@gmail.com>
2023-12-05 12:17:08 -0800

    Adding my project

---
+++ prompt1.py
@@ -0,0 +1,8 @@
+adj1 = input("Adjective 1: ")
+verb1 = input("Verb 1: ")
+verb2 = input("Verb 2 1: ")
+famous_person1 = input("Famous Person 1: ")
+madlib = f"When I think about computer science, I become so {adj1}.\
+The first thing Hercules did is {verb1}, right before the final {verb2}.\
+Then walked in {famous_person1}... just in time to show him the burnt toast."
+print(madlib)
